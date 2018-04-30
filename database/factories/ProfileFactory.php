17dc72e75f9aace20bc9d867e1db37bb192a781f
ohmygodkr <kampanat.ru@ku.th>
2018-04-30 14:06:36 +0700

    test

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -1,4 +1,3 @@
-<<<<<<< current
 <?php
 
 use Faker\Generator as Faker;
@@ -18,25 +17,3 @@ $factory->define(App\Profile::class, function (Faker $faker) {
       'coin' => $faker->numberBetween($min=0, $max= 100)
     ];
 });
-=======
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Profile::class, function (Faker $faker) {
-
-    $users = App\User::all()->pluck('id')->toArray();
-
-    return [
-      'user_id' => $faker->unique()->randomElement($users),
-      'fname' => $faker->firstName,
-      'lname' => $faker->lastName,
-      'birthday' => $faker->date($format = 'Y-m-d', $max = '-5 years'),
-      'sex' => $faker->randomElement(['male', 'female']),
-      'tel' => $faker->phoneNumber,
-      'address' => $faker->address,
-      'coin' => $faker->numberBetween($min=0, $max= 100),
-      'image_path' => '/img/default-avatar.png',
-    ];
-});
->>>>>>> before discard
