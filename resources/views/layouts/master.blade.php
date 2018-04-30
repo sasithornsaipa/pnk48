9c36ba59b9efbed39b940c5b55b1b9ba57bc4e5d
5710404306 <chanidapa14870@gmail.com>
2018-04-30 20:14:03 +0700

    layout got link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,20 +19,20 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="#">PMK48</a>
       @if ( Auth::check() )
-      Hello, {{ Auth::user()->username }}
+      Hello, {{ Auth::user()->username}} : {{Auth::user()->profile->coin}} <i class="fab fa-bitcoin"></i>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Personal Message</a>
+            <a class="nav-link" href="">Personal Message</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Events</a>
+            <a class="nav-link" href="events">Events</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Books</a>
+            <a class="nav-link" href="sales">Books</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/profile/{{ \Auth::user()->profile->id}}">Profile</a>
@@ -61,6 +61,9 @@
       @else
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+          </li>
           <li class="nav-item">
             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></a>
           </li>
