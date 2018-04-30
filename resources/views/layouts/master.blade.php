0ed712423f59731d3f78c0afaa0e218769abf5cf
5710404306 <chanidapa14870@gmail.com>
2018-05-01 00:59:19 +0700

    add personal link

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -26,7 +26,7 @@
             <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="">Personal Message</a>
+            <a class="nav-link" href="/personal_messages">Personal Message</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/events">Events</a>
