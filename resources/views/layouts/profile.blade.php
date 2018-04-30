33c4478a0fa24e8794120d4834ea3fe088677cfd
5710404306 <chanidapa14870@gmail.com>
2018-04-30 20:00:58 +0700

    add link ,edit profile blade

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -1,12 +1,7 @@
+@extends('layouts.master')
 
-
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <style>
+@push('style')
+   <style>
         * {
             box-sizing: border-box;
         }
@@ -18,14 +13,15 @@
         .column {
             float: left;
             padding: 10px;
-            height: 300px; /* Should be removed. Only for demonstration */
+            /* height: 300px; Should be removed. Only for demonstration */
         }
         .left {
             width: 25%;
-            
+            padding: 10px;
         }
         .right {
             width: 75%;
+            padding: 10px;
         }
         /* Clear floats after the columns */
         .row:after {
@@ -45,31 +41,21 @@
             border-radius: 10px;
         } */
     </style>
-  
-  </head>
-  <body>
+@endpush  
 
-    <nav class="navbar navbar-light bg-light justify-content-between">
-        <a class="navbar-brand">PMK48</a>
 
-        <a href="/profile/2">profile</a>
-        <a href="/shelfbook">shef book</a>
-        <a>personal message</a>
-        <a>inbox</a>
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
-    </nav>
-
+@section('content')
     <div class="row">
         <div class="column left">
             <ul class="list-group">
-                <li class="list-group-item">
-                    <a href="/profile/2"><span class="col 3" style="font-weight: bold">My profile</span></a>
+                <li class="list-group-item white-panel">
+                    <a href="/profile/{{ \Auth::user()->profile->id}}"><span class="col 3" style="font-weight: bold">My profile</span></a>
                 </li>
-                <li class="list-group-item">
-                    <a href="/profile/2/buy"><span class="col 3" style="font-weight: bold">ประวัติการซื้อของฉัน</span></a>
+                <li class="list-group-item white-panel">
+                    <a href="/profile/{{ \Auth::user()->profile->id}}/buy"><span class="col 3" style="font-weight: bold">ประวัติการซื้อของฉัน</span></a>
                 </li>
-                <li class="list-group-item">
-                    <a href="/profile/2/sell"><span class="col 3" style="font-weight: bold">ประวัติการขายของฉัน</span></a>
+                <li class="list-group-item white-panel">
+                    <a href="/profile/{{ \Auth::user()->profile->id}}/sell"><span class="col 3" style="font-weight: bold">ประวัติการขายของฉัน</span></a>
                 </li>
             </ul>
         </div>
@@ -79,10 +65,4 @@
             </div>
         </div>
     </div>
-
-
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>
+@endsection
